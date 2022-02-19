326678003debb54af788cb524ca454e2defd863d
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-02-19 15:01:05 +0300

    finish qot

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#menu-toggle').click(function() {
+        $('#sidebar-wrapper').toggleClass('toggled');
+        $('.animated-icon').toggleClass('toggled');
+    });
+});
